a231d0b8d583ec529ba4f7cf726aab4daa476135
SeongJae Park <sj@kernel.org>
2025-06-18 16:34:48 -0700

    run_tests.sh: collect log
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- run_tests.sh
+++ run_tests.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
+log_dir="logs/$(date +%Y-%m-%d-%H-%M-%S)
+mkdir -p "$log_dir"
+
 echo "no memory pressure"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 3G N 1 none none none 120
+sudo ./run.sh ../masim /sys/fs/cgroup/ 3G N 1 none none none 120 | tee "$log_dir/0"
 
 echo "zswap disabled"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 2G N 1 none none none 120
+sudo ./run.sh ../masim /sys/fs/cgroup/ 2G N 1 none none none 120 | tee "$log_dir/1-1"
 
 echo
 echo "zswap disabled, cold data is incompressbile"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 2G N 1 none none /dev/urandom 120
+sudo ./run.sh ../masim /sys/fs/cgroup/ 2G N 1 none none /dev/urandom 120 | tee "$log_dir/1-2"
 
 echo
 echo "zswap enabled"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 2G Y 1 none none none 120
+sudo ./run.sh ../masim /sys/fs/cgroup/ 2G Y 1 none none none 120 | tee "$log_dir/2-1"
 
 echo
 echo "zswap enabled, cold data is incompressbile"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 2G Y 1 none none /dev/urandom 120
+sudo ./run.sh ../masim /sys/fs/cgroup/ 2G Y 1 none none /dev/urandom 120 | tee "$log_dir/2-2"
 
 echo
 echo "zswap enabled, writeback disabled"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 2G Y 0 none none none 120
+sudo ./run.sh ../masim /sys/fs/cgroup/ 2G Y 0 none none none 120 | tee "$log_dir/3-1"
 
 echo
 echo "zswap enabled, writeback disabled, cold data is incompressbile"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 2G Y 1 none none /dev/urandom 120
+sudo ./run.sh ../masim /sys/fs/cgroup/ 2G Y 1 none none /dev/urandom 120 | tee "$log_dir/3-2"
