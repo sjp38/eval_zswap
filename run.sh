bcf0681f6623ddb30508532a68e5050a917350ba
SeongJae Park <sj@kernel.org>
2025-07-30 14:00:10 -0700

    run.sh: update for simpler but easy-to-tune interface
    Make the test easier to control, with below changes.
    
    - Use three 500 MiB sized regions of same access frequency.
    - Control incompressible pages saving feature on/off.
    - Turn off and on swap swap before starting run.
    - Trun off zswap before starting run.
    
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ $# -ne 9 ]
 then
-	echo "Usage: $0 <masim dir> <cgroup mount path> <mem.high> <zswap enable> <zswap writeback> <hot data> <warm data> <cold data> <runtime_seconds>"
+	echo "Usage: $0 <masim dir> <cgroup mount path> <mem.high> <zswap enable> <save incompressbile pages> <hot data> <warm data> <cold data> <runtime_seconds>"
 	echo "e.g., $0 ../masim/ /sys/fs/cgroup/ 2G Y 0 none none /dev/urandom 60"
 	exit 1
 fi
@@ -20,30 +20,33 @@ masim_dir=$1
 cgroup_dir="${2}/eval_zswap"
 mem_high=$3
 zswap_enabled=$4
-zswap_writeback=$5
-hot_data=$6
-warm_data=$7
-cold_data=$8
+zswap_save_incompressible_pages=$5
+first_data=$6
+second_data=$7
+third_data=$8
 runtime_seconds=$9
+
+echo N > /sys/module/zswap/parameters/enabled
+swapoff -a
+swapon /dev/vda5
+echo 3 > /proc/sys/vm/drop_caches
 
 mkdir "$cgroup_dir"
 echo $$ > "${cgroup_dir}/cgroup.procs"
 echo "$mem_high" > "${cgroup_dir}/memory.high"
 
 echo "$zswap_enabled" > "/sys/module/zswap/parameters/enabled"
-echo "$zswap_writeback" > "${cgroup_dir}/memory.zswap.writeback"
-
-echo 3 > /proc/sys/vm/drop_caches
+echo "$zswap_save_incompressible_pages" > "/sys/module/zswap/parameters/save_incompressible_pages"
 
 "$bindir/stat.py" > before_masim
 
 "${masim_dir}/masim.py" run --masim_bin "${masim_dir}/masim" \
 	--phase one $((runtime_seconds * 1000)) \
-	--region hot 1G "$hot_data" --region warm 1G "$warm_data" \
-	--region cold 100M "$cold_data" \
-	--access_pattern one hot 1 4k 70 rw \
-	--access_pattern one warm 1 4k 20 rw \
-	--access_pattern one cold 1 4k 10 rw \
+	--region first 500M "$first_data" --region second 500M "$second_data" \
+	--region third 500M "$third_data" \
+	--access_pattern one first 1 4k 100 rw \
+	--access_pattern one second 1 4k 100 rw \
+	--access_pattern one third 1 4k 100 rw \
 	--log_interval 5000 \
 	--accesses_per_region_selection 1
 
