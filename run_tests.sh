abc24e473f8a320c11b8525a05f0afa6b16a60bf
SeongJae Park <sj@kernel.org>
2025-08-04 11:51:20 -0700

    run_tests: enable zswap shrinker always
    Signed-off-by: SeongJae Park <sj@kernel.org>

--- run_tests.sh
+++ run_tests.sh
@@ -23,13 +23,13 @@ sudo ./run.sh ../masim /sys/fs/cgroup/ 1350M N N N \
 
 echo
 echo "zswap enabled, 10 % memory pressure"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 1350M Y N N \
+sudo ./run.sh ../masim /sys/fs/cgroup/ 1350M Y Y N \
 	./compressible_data ./compressible_data /dev/urandom 120 | \
 	tee "$log_dir/1-2"
 
 echo
 echo "zswap enabled with keeping incompressible pages, 10 % memory pressure"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 1350M Y N Y \
+sudo ./run.sh ../masim /sys/fs/cgroup/ 1350M Y Y Y \
 	./compressible_data ./compressible_data /dev/urandom 120 | \
 	tee "$log_dir/1-3"
 
@@ -41,12 +41,12 @@ sudo ./run.sh ../masim /sys/fs/cgroup/ 1200M N N N \
 
 echo
 echo "zswap enabled, 20 % memory pressure"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 1200M Y N N \
+sudo ./run.sh ../masim /sys/fs/cgroup/ 1200M Y Y N \
 	./compressible_data ./compressible_data /dev/urandom 120 | \
 	tee "$log_dir/2-2"
 
 echo
 echo "zswap enabled with keeping incompressible pages, 20 % memory pressure"
-sudo ./run.sh ../masim /sys/fs/cgroup/ 1200M Y N Y \
+sudo ./run.sh ../masim /sys/fs/cgroup/ 1200M Y Y Y \
 	./compressible_data ./compressible_data /dev/urandom 120 | \
 	tee "$log_dir/2-3"
